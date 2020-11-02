42534d362fafee89a9bc71e44db8fe942a20c30c
MurrayCode <Murraystewart94@hotmail.co.uk>
2020-11-02 11:32:39 +0000

    Go Maps

---
+++ Dictionary/Dictionary.go
@@ -0,0 +1,63 @@
+package Dictionary
+
+//Dictionary map
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+//Error messages
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+//Search Dictionary
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+//Delete Dictionary wordgit add .
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
+//Update Dictionary
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+//Add word to dictionary
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
